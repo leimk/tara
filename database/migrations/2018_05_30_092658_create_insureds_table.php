bc86222fb19985ac56491b2e30aa64a2017c1787
Michael K Lei <michaelkusliawan@gmail.com>
2018-05-30 16:50:08 +0700

    Create Table Migrations for rates and insureds

---
+++ database/migrations/2018_05_30_092658_create_insureds_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInsuredsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insureds', function (Blueprint $table) {
+            $table->increments('idPesertaTaralite');
+            $table->integer('noKontrak');
+            $table->double('besaranPinjaman');
+            $table->date('periodeAwal');
+            $table->date('periodeAkhir');
+            $table->string('namaPeserta',200);
+            $table->date('tglLahir');
+            $table->string('noKTP',20);
+            $table->text('alamat');
+            $table->char('noTel',15);
+            $table->integer('rate_id')->unsigned();
+            $table->foreign('rate_id')->references('idRate')->on('rates');
+            $table->string('key')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insureds');
+    }
+}
